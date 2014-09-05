f202c655f5e5fad214c66e589d5bc1c43995e805
littler.chicken <littler.chicken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-05 12:49:30 +0000

    tagging version 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-listings-genesis/tags/1.3.0@981730 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/archive-listing.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Simple Listing Post Type: Archive/Taxonomy Listing View
+ *
+ * @package    Simple_Listing_Post_Type
+ * @author     Robin Cornett <hello@robincornett.com>
+ * @copyright  2014 Robin Cornett
+ *
+ */
+
+add_filter( 'body_class', 'simplelisting_body_class' );
+function simplelisting_body_class( $classes ) {
+	$classes[] = 'simple-listing';
+
+	return $classes;
+}
+
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+add_action( 'genesis_loop', 'simplelisting_archive_loops' );
+
+/**
+ * Chooses which archive loop to run, HTML5 or XHTML.
+ *
+ * @since  1.3.0
+ *
+ * @return genesis archive loop
+ */
+function simplelisting_archive_loops() {
+	if ( genesis_html5() ) {
+		simplelisting_loop_html5();
+	}
+	else {
+		simplelisting_loop_xhtml();
+	}
+}
+
+/**
+ * Simple Listings HTML5 loop
+ *
+ * @since  1.3.0
+ *
+ * @return HTML5 loop
+ */
+function simplelisting_loop_html5() {
+
+	global $post;
+
+	echo '<div class="listings">';
+	if ( have_posts() ) :
+
+		while ( have_posts() ) : the_post();
+
+		$status = get_the_term_list( $post->ID, 'status', '', ', ', '' ); ?>
+
+			<article <?php post_class(); ?>><div class="listing-wrap"> <?php
+				$image    = get_the_post_thumbnail( $post->ID, 'listing-photo', array( 'class' => 'aligncenter', 'alt' => get_the_title(), 'title' => get_the_title() ) );
+				$fallback = plugins_url( 'includes/sample-images/simple-listings.png' , dirname( __FILE__ ) );
+
+				if ( $image ) {
+					echo '<a href="' . get_permalink() . '">' . $image;
+				}
+				else {
+					printf(
+						'<a href="%s"><img src="%s" class="aligncenter" alt="%s" title="%s" />',
+						get_permalink(),
+						$fallback,
+						the_title_attribute( 'echo=0' ),
+						the_title_attribute( 'echo=0' )
+					);
+				}
+
+				echo genesis_html5() ? '<header class="entry-header">' : '';
+				printf( '<h2 class="entry-title">%s</h2>', the_title_attribute( 'echo=0' ) );
+
+				if ( $status ) {
+					echo '<span class="listing-status">' . strip_tags( $status ) . '</span>';
+				}
+				echo genesis_html5() ? '</header>' : '';
+
+			echo '</a></div></article>';
+
+		endwhile;
+		do_action( 'genesis_after_endwhile' );
+	endif;
+	echo '</div>';
+}
+
+/**
+ * Simple Listings XHTML loop
+ *
+ * @since  1.3.0
+ *
+ * @return XHTML loop
+ */
+function simplelisting_loop_xhtml() {
+
+	global $post;
+
+	echo '<div class="listings">';
+	if ( have_posts() ) :
+
+		while ( have_posts() ) : the_post();
+
+		$status = get_the_term_list( $post->ID, 'status', '', ', ', '' );
+
+			printf( '<div class="%s">', join( ' ', get_post_class() ) );
+			echo '<div class="listing-wrap">';
+				$image    = get_the_post_thumbnail( $post->ID, 'listing-photo', array( 'class' => 'aligncenter', 'alt' => get_the_title(), 'title' => get_the_title() ) );
+				$fallback = plugins_url( 'includes/sample-images/simple-listings.png' , dirname( __FILE__ ) );
+
+				if ( $image ) {
+					echo '<a href="' . get_permalink() . '">' . $image;
+				}
+				else {
+					printf(
+						'<a href="%s"><img src="%s" class="aligncenter" alt="%s" title="%s" />',
+						get_permalink(),
+						$fallback,
+						the_title_attribute( 'echo=0' ),
+						the_title_attribute( 'echo=0' )
+					);
+				}
+
+				printf( '<h2 class="entry-title">%s</h2>', the_title_attribute( 'echo=0' ) );
+
+				if ( $status ) {
+					echo '<span class="listing-status">' . strip_tags( $status ) . '</span>';
+				}
+
+			echo '</a></div></div>';
+
+		endwhile;
+		do_action( 'genesis_after_endwhile' );
+	endif;
+	echo '</div>';
+}
+
+genesis();
---
+++ views/single-listing.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Simple Listing Post Type: Single Listing View
+ *
+ * @package    Simple_Listing_Post_Type
+ * @author     Robin Cornett <hello@robincornett.com>
+ * @copyright  2014 Robin Cornett
+ *
+ */
+
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_before_post_content', 'genesis_post_info' );
+
+remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+remove_action( 'genesis_post_content', 'genesis_do_post_content' );
+add_action( 'genesis_entry_content', 'simplelisting_single_content' );
+add_action( 'genesis_post_content', 'simplelisting_single_content' );
+
+function simplelisting_single_content() {
+	global $post;
+
+	$location    = get_post_meta( $post->ID, '_cmb_listing-location', true );
+	$price       = get_post_meta( $post->ID, '_cmb_listing-price', true );
+	$description = get_the_content();
+	$mls         = get_post_meta( $post->ID, '_cmb_mls-link', true );
+
+	echo get_the_post_thumbnail( $post->ID, 'listing-photo', array( 'class' => 'alignright', 'alt' => the_title_attribute( 'echo=0' ), 'title' => the_title_attribute( 'echo=0' ) ) );
+	if ( $description ) {
+		echo wpautop( '<strong>' . __( 'Description: ', 'simple-listings-genesis' ) . '</strong>' . $description );
+	}
+	if ( $location ) {
+		echo '<strong>' . __( 'Location: ', 'simple-listings-genesis' ) . '</strong> ' . $location;
+	}
+	if ( $price ) {
+		echo '<br />';
+		echo '<strong>' . __( 'Transaction Amount: ', 'simple-listings-genesis' ) . '</strong>' . $price;
+	}
+	if ( $mls ) {
+		echo '<br />';
+		echo '<a href="' . esc_url( $mls ) . '" target="_blank">' . __( 'Listing Details', 'simple-listings-genesis' ) . '</a>';
+	}
+
+}
+
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
+
+genesis();
